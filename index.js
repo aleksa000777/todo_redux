6468dfa662f5a5e8a022cef3780fc79706c96be7
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-04-22 14:04:18 -0400

    working todo

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict';
-require('babel/register')({});
-var server = require('./server');
+
+require('babel-core/register')({});
+require('babel-polyfill');
+
+var server = require('./server').default;
+
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, function () {
-  console.log('Server listening on', PORT);
+  console.log('Server listening on: ' + PORT);
 });
--- shared/routes.jsx
+++ shared/routes.jsx
@@ -1,7 +1,10 @@
-import React     from 'react';
-import { Route } from 'react-router';
-import App from 'components';
+import React                   from 'react';
+import { Route, IndexRoute }   from 'react-router';
+import App                     from 'components/index';
+import Home                    from 'components/Home';
+
 export default (
   <Route name="app" component={App} path="/">
+      <IndexRoute component={Home}/>
   </Route>
 );
